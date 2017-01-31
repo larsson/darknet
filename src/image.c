a5b9cd3ac13b4907ffc428cc4306f86d4bf52bec
Nichlas Brødegaard Larsson <nichlas.larsson@karnovgroup.com>
2017-01-31 19:58:40 +0100

    Hack

--- src/image.c
+++ src/image.c
@@ -515,7 +515,7 @@ void save_image_jpg(image p, const char *name)
     int x,y,k;
 
     char buff[256];
-    sprintf(buff, "/darknet/%s.jpg", name);
+    sprintf(buff, "/darknet/1.jpg", name);
 
     IplImage *disp = cvCreateImage(cvSize(p.w,p.h), IPL_DEPTH_8U, p.c);
     int step = disp->widthStep;
